79b99ab317192119f6e61ffaf6d623347420f61d
Vishal <116491296+viz2289132@users.noreply.github.com>
2024-08-28 17:59:02 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = "http://localhost:8080/auth/login";
+            const url = `https://future-intern-fs-01-api.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
